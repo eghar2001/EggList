6ba7528910e1c2753341dfb97bbf1cc694f85113
Nahuel Coronel <nahucarc2@gmail.com>
2024-02-08 12:59:00 -0300

    cambio a pymysql

--- eggList/__init__.py
+++ eggList/__init__.py
@@ -5,7 +5,6 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
-
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 migrate = Migrate()
--- eggList/config.py
+++ eggList/config.py
@@ -11,3 +11,4 @@ MAIL_PORT = os.getenv("MAIL_PORT")
 MAIL_USE_TLS = os.getenv("MAIL_USE_TLS")
 MAIL_USERNAME = os.getenv("MAIL_USERNAME")
 MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
